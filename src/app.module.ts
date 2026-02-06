988be3a13818987633e08d695b69cc73132d2963
hazzaz-am <hazzazabdulmannan@gmail.com>
2026-02-06 22:31:16 +0600

    feat: add SupabaseAuthGuard and corresponding tests; update app module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,25 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ExceptionController } from './exception/exception.controller';
-import { LoggerMiddlewareMiddleware } from './middleware/logger-middleware/logger-middleware.middleware';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
-import { ProductModule } from './product/product.module';
-import { LibraryModule } from './library/library.module';
-import { ProjectModule } from './project/project.module';
-import { EmployeeModule } from './employee/employee.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { StudentModule } from './student/student.module';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ExceptionController } from "./exception/exception.controller";
+import { LoggerMiddlewareMiddleware } from "./middleware/logger-middleware/logger-middleware.middleware";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserModule } from "./user/user.module";
+import { ConfigModule } from "@nestjs/config";
+import { ProductModule } from "./product/product.module";
+import { LibraryModule } from "./library/library.module";
+import { ProjectModule } from "./project/project.module";
+import { EmployeeModule } from "./employee/employee.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { StudentModule } from "./student/student.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: "postgres",
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
@@ -35,6 +37,6 @@ import { StudentModule } from './student/student.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddlewareMiddleware).forRoutes('*');
+    consumer.apply(LoggerMiddlewareMiddleware).forRoutes("*");
   }
 }
