a527b1a6dd2bb9961f7ee7ee03020ae75074b8ae
hazzaz-am <hazzazabdulmannan@gmail.com>
2026-05-09 08:37:44 +0600

    feat: initialize NestJS application with basic structure and configuration

--- src/guards/roles/roles.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum ERoles {
-  USER = 'USER',
-  ADMIN = 'ADMIN',
-}
--- src/library/dto/book.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class BookDto {
-  @IsString()
-  title: string;
-
-  @IsString()
-  author: string;
-}
